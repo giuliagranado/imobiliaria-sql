f07aea748df6804c9b7ef2c6a6bba85b8a5f6d36
Giulia Granado <giulia.granado@gmail.com>
2026-04-16 18:40:16 -0300

    Update total_apartamento_vw.sql

--- views/total_apartamento_vw.sql
+++ views/total_apartamento_vw.sql
@@ -1,17 +1,11 @@
 
--- Estrutura stand-in para vista `total_apartamento_vw`
--- (Veja abaixo para a view atual)--
-
-CREATE TABLE `total_apartamento_vw` (
-`codigo_cond` int(5)
-,`total_apartamento` bigint(21)
-);
-
 -- Estrutura para vista `total_apartamento_vw`--
 DROP VIEW IF EXISTS `total_apartamento_vw`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `total_apartamento_vw`  AS 
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` 
+SQL SECURITY DEFINER 
+VIEW `total_apartamento_vw`  AS 
 SELECT `apartamento`.`codigo_cond` AS `codigo_cond`, 
-count(0) AS `total_apartamento` 
+      count(0) AS `total_apartamento` 
 FROM `apartamento` 
 GROUP BY `apartamento`.`codigo_cond``codigo_cond`  ;
