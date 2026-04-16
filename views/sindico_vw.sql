63a964fdc0b78b5584e7bf1de2b9e29af7cfa390
Giulia Granado <giulia.granado@gmail.com>
2026-04-16 18:39:13 -0300

    Update sindico_vw.sql

--- views/sindico_vw.sql
+++ views/sindico_vw.sql
@@ -1,13 +1,10 @@
 
--- Estrutura stand-in para vista `sindico_vw`
--- (Veja abaixo para a view atual)--
-
-CREATE TABLE `sindico_vw` (
-`nome` varchar(80)
-,`telefone` varchar(15)
-);
-
 -- Estrutura para vista `sindico_vw`--
 DROP VIEW IF EXISTS `sindico_vw`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `sindico_vw`  AS SELECT `sindico`.`nome` AS `nome`, `sindico`.`telefone` AS `telefone` FROM `sindico``sindico`  ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` 
+  SQL SECURITY DEFINER 
+  VIEW `sindico_vw`  AS 
+  SELECT `sindico`.`nome` AS `nome`, 
+        `sindico`.`telefone` AS `telefone` 
+  FROM `sindico``sindico`  ;
