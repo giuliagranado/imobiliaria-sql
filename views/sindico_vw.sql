27672ac4fdda5546d8a2906edd724be38f898028
Giulia Granado <giulia.granado@gmail.com>
2026-04-16 18:33:56 -0300

    Update sindico_vw.sql to drop view instead of table

--- views/sindico_vw.sql
+++ views/sindico_vw.sql
@@ -8,6 +8,6 @@ CREATE TABLE `sindico_vw` (
 );
 
 -- Estrutura para vista `sindico_vw`--
-DROP TABLE IF EXISTS `sindico_vw`;
+DROP VIEW IF EXISTS `sindico_vw`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `sindico_vw`  AS SELECT `sindico`.`nome` AS `nome`, `sindico`.`telefone` AS `telefone` FROM `sindico``sindico`  ;
